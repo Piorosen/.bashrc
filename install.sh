1f89aacb1fd092f8e0ec6c9384f93aeb2d4249f2
JooHyoung Cha <chacha@udon.party>
2024-08-27 20:58:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 export ZPLUG_HOME=~/.zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 ln .zshrc ~/.zshrc
+ln .hyper.js ~/.hyper.js
+ln .p10k.zsh ~/.p10k.zsh
+ln .vimrc ~/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 source ~/.zshrc
 
 
